6bb3f16bc1bb1a5a9bd1c94a06c6a2b7be7db73c
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2023-04-01 21:43:26 -0400

    commit #2 added slices

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,7 +1,7 @@
 function CarForm(){
     return(
-        <div>
-            CarForm!
+        <div className="car-form panel">
+            <h4 className="subtitle is-3">Add car</h4>
         </div>
     )
 }
